5f1179269f49470c40b74d8d33cfc351a3cef523
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-01-02 08:50:54 -0500

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ newHeader.setAttribute('ID', 'victory')
 // using innerHTML
 let element = document.querySelector('h1#victory')
 element.innerHTML = 'ebo is the champion'
+
+// better solution!!!
+
+const testVar = "hi"
+
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
