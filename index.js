4de24b14cfa515efabc322a77ac7b41ea2811112
mfioran1 <maurice97f@gmail.com>
2021-01-11 19:55:33 -0800

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 document.querySelector('main#main').remove()
 
-let newHeader = document.createElement('h1');
-newHeader.id = 'victory';
+let newHeader = document.createElement('h1#victory');
+
 newHeader.innerHTML = 'Maurice is the champion'
 
 
